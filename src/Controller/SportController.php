c3d99e237e1bf1f6b3b318f0cd626b18e65451d1
Olga21-12 <olgapetrenko.brxl@gmail.com>
2025-05-02 11:33:58 +0200

    end

--- src/Controller/SportController.php
+++ src/Controller/SportController.php
@@ -2,23 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Sport;
+use App\Repository\SportRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class SportController extends AbstractController
 {
     #[Route('/sports', name: 'sport_index')]
-public function index(SportRepository $sportRepository): Response
-{
-    return $this->render('sport/index.html.twig', [
-        'sports' => $sportRepository->findAll()
-    ]);
-}
-
-    #[Route('/sport/{id}', name: 'sport_show')]
-        public function show(Sport $sport): Response
+    public function index(SportRepository $sportRepository): Response
     {
-        return $this->render('sport/show.html.twig', ['sport' => $sport]);
+        $sports = $sportRepository->findAll();
+
+        return $this->render('sport/index.html.twig', [
+            'sports' => $sports,
+        ]);
+    }
+
+    #[Route('/sport/{id}', name: 'sport_show', requirements: ['id' => '\d+'])]
+    public function show(Request $request, int $id, SportRepository $sportRepository): Response
+    {
+        $sport = $sportRepository->find($id);
+
+        if (!$sport) {
+            throw $this->createNotFoundException('Sport non trouvé');
+        }
+
+        return $this->render('sport/show.html.twig', [
+            'sport' => $sport,
+        ]);
     }
 }
+
